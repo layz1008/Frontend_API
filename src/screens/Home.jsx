91a9c86adee2668eb7f9b1be4bee51feb522a11c
layz1008 <zlay1008@gmail.com>
2023-01-22 13:24:41 -0500

    Background image added to Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>League of Legends!!!!</div>
+    
+        <div className="home">
+        </div>
   )
-}
+  }
